9604f794f0e486a56692e77ab954a2ca85e38170
Gregtom3 <gamatousek@gmail.com>
2025-07-29 14:54:43 -0400

    Need true info for rec MC trees

--- src/modules/filterTree.C
+++ src/modules/filterTree.C
@@ -5,8 +5,11 @@
 #include <iostream>
 #include <yaml-cpp/yaml.h>
 
-const std::vector<std::string>& keepBranches = {"x",     "Q2",     "y",  "hel", "eps", "Mh", "M2",   "Pol",
-                                                "phi_h", "phi_R1", "th", "z",   "xF",  "Mx", "pTtot"};
+const std::vector<std::string>& keepBranches = {"x",         "Q2",         "y",      "hel",   "eps",     "Mh",     "M2",
+                                                "Pol",       "phi_h",      "phi_R1", "th",    "z",       "xF",     "Mx",
+                                                "pTtot",     "truex",      "trueQ2", "truey", "trueeps", "trueMh", "trueM2",
+                                                "truephi_h", "truephi_R1", "trueth", "truez", "truexF",  "trueMx", "truepTtot",
+                                                "truepid_1", "truepid_2", "truepid_21", "truepid_22"};
 
 void filterTree(const char* inputPath, const char* treeName, const char* configPath, const char* pairName, const char* outputDir,
                 Int_t maxEntries = -1) {
@@ -24,15 +27,16 @@ void filterTree(const char* inputPath, const char* treeName, const char* configP
     // 2a) Open input
     TFile* inF = TFile::Open(inputPath, "READ");
     TTree* inT = (TTree*)inF->Get(treeName);
-
+    const bool isMC = std::string(inputPath).find("MC_") != std::string::npos;
     // 2b) Keep only desired branches  ---------------------------------------
     inT->SetBranchStatus("*", 0); // disable all
     if (keepBranches.empty()) {
         std::cerr << "WARNING: keepBranches is empty; output tree will be empty\n";
     }
-    for (const auto& br : keepBranches)
-        inT->SetBranchStatus(br.c_str(), 1); // enable requested ones
-
+    for (const auto& br : keepBranches){
+        if (!isMC && br.find("true") != std::string::npos) continue; // Skip true data for non-MC
+        inT->SetBranchStatus(br.c_str(), 1);
+    }
     // 3) Prepare output file *first* and cd into it
     std::string fname = gSystem->BaseName(inputPath);
     gSystem->mkdir(outputDir, kTRUE);
